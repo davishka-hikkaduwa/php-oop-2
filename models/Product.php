77d86451ee431e29a94eb0e7841f0df07b79b6fd
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2022-10-27 10:00:47 +0200

    product model

---
+++ models/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+class Product {
+    public $price = 0;
+    public $vat = 22;
+    public $brand;
+    public $name;
+    public $description = '';
+    public $category;
+
+    function __construct($_brand, $_name, $_price){
+        $this->brand = $brand;
+        $this->name = $_name;
+        $this->price = $_price;
+    }
+}
+
+?>
