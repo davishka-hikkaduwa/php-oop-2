2ad4474eaaf75c68026ceb2a80fba2b7b12239ab
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2022-10-27 10:00:57 +0200

    foodProduct model

---
+++ models/FoodProduct.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once __DIR__ . '/Product.php';
+
+class FoodProduct extends Product{
+    public $category = 'Food';
+    public $expiry_date = date('Y-M-D');
+    public $type = 'umido'; // umido | secco
+    public $weight = 0;
+    public $ingredients = [];
+    public $forPetWithWeight = 0;
+
+}
+
+?>
